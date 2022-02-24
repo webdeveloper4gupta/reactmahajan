728a094af1e9617099d824837b3dce985984230d
SUKRITAN GUPTA <20051871@kiit.ac.in>
2022-02-24 22:31:00 +0530

    Updates

---
+++ precache-manifest.f68de85f5b9b28e4cbb5f00dae045e2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dffc9bc545d73e554c6e",
+    "url": "/reactmahajan/static/css/main.a55584ba.chunk.css"
+  },
+  {
+    "revision": "dffc9bc545d73e554c6e",
+    "url": "/reactmahajan/static/js/main.dffc9bc5.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/reactmahajan/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/reactmahajan/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "bda79f0bda016852471b",
+    "url": "/reactmahajan/static/js/runtime~main.bda79f0b.js"
+  },
+  {
+    "revision": "23e36bd955bd3e477df1f0a75c808b2c",
+    "url": "/reactmahajan/index.html"
+  }
+];
